65444cf25307d94a0cfef7966fbb3147a54d782f
John200410 <43681932+John200410@users.noreply.github.com>
2024-02-26 21:55:47 -0500

    1.1 - added 1.20.x support

--- src/main/java/me/john200410/instanceinfo/Plugin.java
+++ src/main/java/me/john200410/instanceinfo/Plugin.java
@@ -12,31 +12,12 @@ public class Plugin extends org.rusherhack.client.api.plugin.Plugin {
 	@Override
 	public void onLoad() {
 		RusherHackAPI.getModuleManager().registerFeature(new InstanceInfoModule());
-		this.getLogger().info(this.getName() + " loaded!");
+		this.getLogger().info("InstanceInfo loaded!");
 	}
 	
 	@Override
 	public void onUnload() {
-		this.getLogger().info(this.getName() + " unloaded!");
+		this.getLogger().info("InstanceInfo unloaded!");
 	}
 	
-	@Override
-	public String getName() {
-		return "InstanceInfo";
-	}
-	
-	@Override
-	public String getVersion() {
-		return "v1.0";
-	}
-	
-	@Override
-	public String getDescription() {
-		return "Adds additional information to the game window's title";
-	}
-	
-	@Override
-	public String[] getAuthors() {
-		return new String[]{"John200410"};
-	}
 }
